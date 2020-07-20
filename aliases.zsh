f00efed82a14a9929c1b3bdc2bbcc156efadbe8a
Michael J. Miller <mikem.exe@gmail.com>
2020-07-20 16:05:03 -0600

    Single-line git log alias

--- aliases.zsh
+++ aliases.zsh
@@ -1,2 +1,7 @@
 alias gh='gitit compare'
 alias branches='git --no-pager branch'
+
+# Git aliases
+
+# ll: Log Line (git log, but prettier and more terse)
+git config --global alias.ll "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
